b8f6eba2d474d9d061dbde9a5bdc0fd483aceda8
Mattathias Lerner <matt@Mattathiass-MacBook-Pro.local>
2019-12-06 10:23:54 -0500

    added function for geocode merge

--- ses/block_ses.py
+++ ses/block_ses.py
@@ -2,6 +2,7 @@ import requests
 import os
 import pandas as pd
 import json
+import numpy as np
 
 """
 
@@ -34,30 +35,29 @@ https://api.census.gov/data/2017/acs/acs5/profile/variables.html
 
 """
 
-class ADI:
+class ADI (object):
 	"""
 	Methods related to the Area Deprivation Statistics
 	Data can be downloaded from: https://www.neighborhoodatlas.medicine.wisc.edu/
+
+	This object, at init, simply reads in the raw textfile of ADI indices at the block group level and forms it into quintiles.
+
+	This currently uses the ADI based on the 2011-2015 ACS 5-year estimates.
+
+	Block groups containing less than 100 persons, 30 housing units, or >33% of pop living in group quarters are suppressed
+	with the value "PH". ADI indices are coerced to numeric, so suppressed indices are coerced to NaN.
+
+	Finally, indices are bucketed into quintiles. IMPORTANT: Lower quintiles are HIGHER-SES. Higher quintiles are more deprived.
 	"""
-	adi_location = "../data/adi-download/US_blockgroup_15.txt"
-
-	def get_adi_frame():
-		"""
-		This function simply reads in the raw textfile of ADI indices at the block group level and forms it into quintiles.
-
-		This currently uses the ADI based on the 2011-2015 ACS 5-year estimates.
-
-		Block groups containing less than 100 persons, 30 housing units, or >33% of pop living in group quarters are suppressed
-		with the value "PH". This function coerces indices to numeric, so suppressed indices are coerced to NaN.
-
-		Finally, indices are bucketed into quintiles. IMPORTANT: Lower quintiles are HIGHER-SES. Higher quintiles are more deprived.
-		"""
-		adi = pd.read_csv(ADI.adi_location, sep=',', dtype="str") # read in ADI by block group from text file
+	def __init__(self):
+		adi_location = "../data/adi-download/US_blockgroup_15.txt"
+		adi = pd.read_csv(adi_location, sep=',', dtype="str") # read in ADI by block group from text file
 		adi['adi_natrank_numeric'] = pd.to_numeric(adi['adi_natrank'], errors='coerce')
 		adi['adi_quintile'] = pd.qcut(adi['adi_natrank_numeric'], [0, 0.2, 0.4, 0.6, 0.8, 1], labels=["0-20","20-40","40-60","60-80","80-100"])
-		return(adi)
+		self.adi_frame = adi
+		return None
 
-	def get_quintile_for_geocode(fips_geocode, adi_frame):
+	def get_quintile_for_geocode(self, fips_geocode):
 		"""
 		Parameters:
 		-----------
@@ -71,8 +71,44 @@ class ADI:
 		-------
 		A single string value such as "0-20" denoting the deprivation percentile of the provided block group.
 		"""
-		slice_ = adi_frame.loc[adi_frame.fips == fips_geocode, 'adi_quintile'].iat[0]
+		slice_ = self.adi_frame.loc[self.adi_frame.fips == fips_geocode, 'adi_quintile'].iat[0]
 		return(slice_)
+
+	def get_quintile_for_geocodes_frame(self, dataframe, geocode_column_name):
+		"""
+		Parameters:
+		-----------
+		dataframe : Pandas Dataframe
+			Dataframe with a column containing geocode
+
+		geocode_column_name : str
+			Name of column in dataframe containing geocodes
+
+		Returns
+		-------
+		The original dataframe with an "adi_quintile" column containing the ADI quintile.
+
+		Notes
+		-------
+		Geocodes often contain leading zeroes, so be sure that the input column is correctly formatted! It should be an object or str.
+		"""
+
+		adi_ranks_only = self.adi_frame[['fips', 'adi_quintile']]
+		geocodes_merged = dataframe.merge(adi_ranks_only, left_on=geocode_column_name, right_on='fips', how='left', indicator=True)
+
+		# count up the merges
+		count_merged = np.sum(geocodes_merged._merge == "both")
+		count_unmerged = len(dataframe) - count_merged
+		print("Geocode merge: Merged {} of {} observations in input dataframe ({}%)".format(str(count_merged), str(count_unmerged), str(round(100*count_merged/len(dataframe), 2))))
+
+		# a little bit of error handline
+		if (count_merged == 0):
+			print ("Merged 0 of {} observations in input dataframe! Make sure that geocodes have been read in the correct format and watch out for the removal of leading zeroes!".format(str(len(dataframe))))
+
+		del geocodes_merged['_merge']
+		del geocodes_merged['fips']
+
+		return(geocodes_merged)
 
 api_key = ""
 
@@ -124,9 +160,8 @@ def get_geocode_for_address(address, city, state_code):
 		return ""		
 
 if __name__ == "__main__":
-	#example_geocode = get_geocode_for_address("800 Ashford St.","Brooklyn","NY")
+	adi = ADI()
+
 	geocode_ = get_geocode_for_address("211 Hoyt Street","Brooklyn","NY")
-	adi_frame = ADI.get_adi_frame()
-	geocode_quintile = ADI.get_quintile_for_geocode(geocode_, adi_frame) #WEHO 060377004001
-	print(type(geocode_quintile))
+	geocode_quintile = adi.get_quintile_for_geocode(geocode_)
 
