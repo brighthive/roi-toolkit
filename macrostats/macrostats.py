5ee2d51f30948ba7fee391df84625cd57c5c901d
Matt Lerner <mattlerner@brighthive.io>
2019-12-09 16:39:34 -0500

    env variable in macrostats

--- macrostats/macrostats.py
+++ macrostats/macrostats.py
@@ -2,10 +2,23 @@ import requests
 import json
 import pandas as pd # using pandas here for the sake of (1) familiarity and (2) ease of extensibility
 import numpy as np
+import os
 
 """
 
 ##### Notes #####
+
+# Main Background:
+https://www.bls.gov/cps/data.htm
+
+# Series ID Formats:
+https://www.bls.gov/help/hlpforma.htm
+
+# Of particular interest is the LE Series
+https://download.bls.gov/pub/time.series/le/le.series
+
+# Compressed flat files
+https://download.bls.gov/pub/time.series/compressed/tape.format/
 
 ###### Important things to know
 
@@ -17,22 +30,27 @@ Registered users may request up to 500 queries daily. Unregistered users may req
 How many series can I include in a query?
 Registered users may request up to 50 series per query. Unregistered users may request up to 25 series per query.
 
-###### BLS API Formats
+###### Figuring out LE series
 
+There is no real documentation for this series - all the BLS offers is this site: https://download.bls.gov/pub/time.series/le/le.series
+So we have to glean what we can from this page, which indicates that the format for these series is:
 
-Data type codes
+LE + seasonal adjustment code[1 - U/S] + ?[1] + lfst_code[2] + ?[5] ?[2]
 
-measure_code	measure_text
-03	unemployment rate
-04	unemployment
-05	employment
-06	labor force
 
 """
 
 example_response = '{"status":"REQUEST_SUCCEEDED","responseTime":137,"message":[],"Results":{\n"series":\n[{"seriesID":"LAUST080000000000006","data":[{"year":"2019","period":"M10","periodName":"October","latest":"true","value":"3178070","footnotes":[{"code":"P","text":"Preliminary."}]},{"year":"2019","period":"M09","periodName":"September","value":"3177825","footnotes":[{}]},{"year":"2019","period":"M08","periodName":"August","value":"3173865","footnotes":[{}]},{"year":"2019","period":"M07","periodName":"July","value":"3189419","footnotes":[{}]},{"year":"2019","period":"M06","periodName":"June","value":"3186284","footnotes":[{}]},{"year":"2019","period":"M05","periodName":"May","value":"3127934","footnotes":[{}]},{"year":"2019","period":"M04","periodName":"April","value":"3119039","footnotes":[{}]},{"year":"2019","period":"M03","periodName":"March","value":"3123249","footnotes":[{}]},{"year":"2019","period":"M02","periodName":"February","value":"3132592","footnotes":[{}]},{"year":"2019","period":"M01","periodName":"January","value":"3118999","footnotes":[{}]},{"year":"2018","period":"M13","periodName":"Annual","value":"3096358","footnotes":[{}]},{"year":"2018","period":"M12","periodName":"December","value":"3136729","footnotes":[{}]},{"year":"2018","period":"M11","periodName":"November","value":"3133401","footnotes":[{}]},{"year":"2018","period":"M10","periodName":"October","value":"3135286","footnotes":[{}]},{"year":"2018","period":"M09","periodName":"September","value":"3124018","footnotes":[{}]},{"year":"2018","period":"M08","periodName":"August","value":"3114611","footnotes":[{}]},{"year":"2018","period":"M07","periodName":"July","value":"3126894","footnotes":[{}]},{"year":"2018","period":"M06","periodName":"June","value":"3115771","footnotes":[{}]},{"year":"2018","period":"M05","periodName":"May","value":"3071539","footnotes":[{}]},{"year":"2018","period":"M04","periodName":"April","value":"3060983","footnotes":[{}]},{"year":"2018","period":"M03","periodName":"March","value":"3056824","footnotes":[{}]},{"year":"2018","period":"M02","periodName":"February","value":"3054983","footnotes":[{}]},{"year":"2018","period":"M01","periodName":"January","value":"3025258","footnotes":[{}]},{"year":"2017","period":"M13","periodName":"Annual","value":"2992412","footnotes":[{}]},{"year":"2017","period":"M12","periodName":"December","value":"3022280","footnotes":[{}]},{"year":"2017","period":"M11","periodName":"November","value":"3027893","footnotes":[{}]},{"year":"2017","period":"M10","periodName":"October","value":"3030716","footnotes":[{}]},{"year":"2017","period":"M09","periodName":"September","value":"3035085","footnotes":[{}]},{"year":"2017","period":"M08","periodName":"August","value":"3019660","footnotes":[{}]},{"year":"2017","period":"M07","periodName":"July","value":"3019734","footnotes":[{}]},{"year":"2017","period":"M06","periodName":"June","value":"3007314","footnotes":[{}]},{"year":"2017","period":"M05","periodName":"May","value":"2966441","footnotes":[{}]},{"year":"2017","period":"M04","periodName":"April","value":"2957983","footnotes":[{}]},{"year":"2017","period":"M03","periodName":"March","value":"2952018","footnotes":[{}]},{"year":"2017","period":"M02","periodName":"February","value":"2945602","footnotes":[{}]},{"year":"2017","period":"M01","periodName":"January","value":"2924216","footnotes":[{}]}]}]\n}}'
 
-BLS_API_KEY = "c8803d0ba66c4592b8b0eff68ac9ebb0" # unnecessary for BLS series 1.0 api but series 2 overcomes extreme rate limiting
+BLS_API_KEY = os.getenv('BLS_API_KEY') # unnecessary for BLS series 1.0 api but series 2 overcomes extreme rate limiting
+
+class CPS_Fetch_Data:
+	def a():
+		return None
+
+class CPS_Calculations:
+	def b():
+		return None
 
 class Parameters:
 	BLS_measure_codes = {
@@ -59,6 +77,10 @@ class BLS_API:
 		series_id = prefix + seasonal_adjustment_code + state_code + area_code + industry_code + data_type_code
 		return series_id
 
+	def hs_grad_series_id():
+		# LEU0252917300 works
+		return series_id
+
 	def get_series(seriesid, startyear, endyear, api_key=BLS_API_KEY):
 		url = ("https://api.bls.gov/publicAPI/v2/timeseries/data/{}?startyear={}&endyear={}&registrationkey={}".format(seriesid, startyear, endyear, api_key))
 		response = requests.get(url)
@@ -80,6 +102,10 @@ class BLS_API:
 		data_frame['month_year'] = pd.to_datetime(data_frame.periodName + " " + data_frame.year, format='%B %Y', errors='coerce').astype('datetime64[M]')
 
 		return(data_frame)
+
+class ACS_API:
+	def earnings_call():
+		return None
 
 class Calculations:
 	"""
@@ -161,7 +187,7 @@ class Calculations:
 
 if __name__ == "__main__":
 
-	'''
+	
 	# series ids
 	employment_series_id = BLS_API.employment_series_id(measure_code="employment")
 	labor_force_series_id = BLS_API.employment_series_id(measure_code="labor force")
@@ -172,6 +198,9 @@ if __name__ == "__main__":
 	labor_force_raw = BLS_API.get_series(labor_force_series_id, 2014, 2018)
 	wage_raw = BLS_API.get_series(wage_series_id, 2014, 2018)
 
+	print(employment_raw)
+	exit()
+
 	# to dataframes
 	employment = BLS_API.parse_api_response(employment_raw)
 	labor_force = BLS_API.parse_api_response(labor_force_raw)
@@ -180,7 +209,7 @@ if __name__ == "__main__":
 	employment.to_pickle('employment_pickled.pkl')
 	labor_force.to_pickle('labor_force_pickled.pkl')
 	wage.to_pickle('wage_pickled.pkl')
-	'''
+	
 	employment = pd.read_pickle('employment_pickled.pkl')
 	labor_force = pd.read_pickle('labor_force_pickled.pkl')
 	wage = pd.read_pickle('wage_pickled.pkl')
