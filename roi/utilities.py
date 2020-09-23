c3107322abe2c8496da0bfd0df4bae253eef93e8
Matt Lerner <mattlerner@brighthive.io>
2020-09-23 11:50:44 -0700

    added Programs class, still needs to be finished with cost

---
+++ roi/utilities.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+class Summaries:
+
+	def summary_by_group(frame_, grouping_factors, column_to_aggregate):
+		grouped = frame_.groupby(grouping_factors, as_index=False)[column_to_aggregate].agg({'n':np.size,'mean':np.mean, 'median':np.median, 'sd':np.std, 'min':np.min, 'max':np.max})
+		return(grouped)
