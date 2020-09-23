42236b50e1788ac4eb5712421f5f03dd7b98b8e8
Matt Lerner <mattlerner@brighthive.io>
2020-09-23 12:19:53 -0700

    added analysis file, empty currently almost

---
+++ roi/analysis.py
@@ -0,0 +1,21 @@
+
+class Cost_Effectiveness:
+	def main():
+		return(None)
+
+class ROI:
+	def main():
+		return(None)
+
+class ROI:
+	def main():
+		return(None)
+
+class Cost_Benefit = ROI
+
+class Equity_Assessment(object):
+	"""
+	This class takes a dataframe and produces objects useful for evaluating equity concerns.
+	"""
+	def __init__(self, dataframe, variable_of_concern, group):
+		return(None)
