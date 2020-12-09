5ed846007727232aee260197ea2dcfb9f9b1be7c
Matt Lerner <mattlerner@brighthive.io>
2020-12-09 11:49:04 -0800

    added first batch of docstringsgit add roi/macro.py roi/equity.py roi/cost.py

--- roi/equity.py
+++ roi/equity.py
@@ -5,7 +5,64 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 import warnings
 
+"""
+
+### Equity metrics ###
+
+This submodule contains methods for calculating equity metrics. Each metric has its own class, inherited from the Metric()
+class, which provides useful attributes for all metrics. Each equity metric coded here is a measure that is generally
+decomposable into within- and between-group inequality. As such, each method also provides two attributes.
+
+(1) is .overall, which provides the value of the index being calculated (Variance, Gini, Theil-L or Theil T). In order to do so,
+each metric calculates the decomposition of both within- and between-group values of the given metric, e.g. between-group Gini or
+within-group Gini. 
+
+(2) is .ratio, which is the ratio of between-group inequality to overall inequality. This is interpretable, broadly speaking,
+as the amount of variation that is accounted for by intergroup variation. As such, it takes a value from 0 to 1, where 0 indicates
+that none of the variation across the provided value is accounted for by intergroup differences. This is an indicator for, or proxy of,
+inequality across groups.
+
+Ideally, variation in individual outcomes is driven only by individual differences, instead of group membership. Therefore, the closer
+the value of .ratio is to 1, the more outcomes seem to be accounted for by group membership, and the "worse" the inequality situation is.
+
+The values of .overall and .ratio should be taken as clues for future investigation of possible inequalities or inequities, not as
+metrics that themselves demonstrate disparate treatment, bias, or discrimination. Intergroup difference is not itself evidence of
+discrimination or bias. However, where discrimination or bias is at play, intergroup difference is likely to appear.
+
+Please note that all methods here IGNORE NA VALUES.
+
+References
+-----------
+https://seer.cancer.gov/help/hdcalc/inference-methods/individual-level-survey-sample-1/measures-of-relative-disparity/theil-index-t
+https://utip.lbj.utexas.edu/papers/utip_14.pdf
+https://www.usi.edu/media/3654811/Analysis-of-Inequality.pdf
+https://en.wikipedia.org/wiki/Theil_index
+http://www.fao.org/3/a-am342e.pdf
+
+### Area Deprivation Index ###
+
+
+"""
+
 class Metric():
+	"""
+	This is equity Metric() parent class from which all the equity child metric classes inherit.
+
+	Parameters:
+		unique_groups	:	A numpy array containing the names or values of different groups within a population e.g. np.array(['Men','Women'])
+		grouped_values	:	A list of numpy arrays, each of which contains values for one and only one group. This list should be ordered according
+							to the value of unique_groups (above), e.g. [np.array([values for men]), np.array(values for women)]
+
+	Attributes:
+		unique_groups			:	The value passed as an argument for unique_groups
+		grouped_values			:	The value passed as an argument for grouped_values
+		ungrouped_observations	:	A single numpy array of all observations, ordered by group
+		n_groups				:	The number of unique groups passed in unique_groups
+		n						:	The number of values across all groups
+		viz						:	A Seaborn boxplot offering a simple visualization of cross-group variation
+		nans					:	The number of NaN values passed to the class
+
+	"""
 	def __init__(self, unique_groups, grouped_values):
 		self.unique_groups = unique_groups
 		self.grouped_values = grouped_values
@@ -23,6 +80,15 @@ class Metric():
 	# factory method
 	@classmethod
 	def from_dataframe(cls, frame, group_columns, value_column, sample=False):
+		"""
+		This factory method allows us to pass a dataframe directly to any equity metric class instead of having to create numpy arrays from it
+
+		Parameters:
+			frame			:	A dataframe
+			group_columns	:	The name of the column that denotes groups, or a list of columns on which to group
+			value_column	:	The name of the column that contains values to be analyzed
+			sample			:	If False, will use the whole dataset. Otherwise, should be a number denoting the size of the sample to take. Defaults to False.
+		"""
 		if sample != False:
 			warnings.warn("When sample == True, Metric() will use the value of sample to create a random subset of values that it will use for all calculations. This number will be stored as attribute .samplesize.")
 			if not isinstance(sample, int):
@@ -36,6 +102,18 @@ class Metric():
 
 	@staticmethod
 	def simple_viz(unique_groups, grouped_values):
+		"""
+		This produces a box plot that allows the analyst to quickly and visually assess differences betweenb groups
+
+		Parameters:
+			unique_groups	:	A numpy array containing the names or values of different groups within a population e.g. np.array(['Men','Women'])
+			grouped_values	:	A list of numpy arrays, each of which contains values for one and only one group. This list should be ordered according
+								to the value of unique_groups (above), e.g. [np.array([values for men]), np.array(values for women)]
+
+		Return:
+			fig				:	A complete Seaborn boxplot that can be displayed or saved to disk
+
+		"""
 		x = np.array(grouped_values)
 		fig, ax = plt.subplots(1,1)
 		plot = sns.boxplot(data = x, labels=[unique_groups], ax = ax, orient='h', showfliers=False) # annotate interpretation
@@ -46,19 +124,12 @@ class Metric():
 
 class Theil_T(Metric):
 	"""
-	Class provides methods for calculating individual terms in the Theil T index, as well as
-	(1) a function for calculating the index itself and
-	(2) a function calculating a ratio denoting the proportion of inequality accounted for by cross-group inequality
+	Class provides methods for calculating individual terms in the Theil T index. See references for more detail.
+	The Theil index is perfectly decomposable into between- and within-group inequality. It works only using
+	positive values, which makes it unsuitable for assessing inequality across values that may take any
+	value across the entire real line (e.g. net worth, or any kind of difference).
 
-	Please note that the Theil index takes only positive values!
-
-	Reference
-	-----------
-	https://seer.cancer.gov/help/hdcalc/inference-methods/individual-level-survey-sample-1/measures-of-relative-disparity/theil-index-t
-
-	https://utip.lbj.utexas.edu/papers/utip_14.pdf
-
-	https://www.usi.edu/media/3654811/Analysis-of-Inequality.pdf
+	Relative to the Theil L, the Theil T metric is sensitive to changes at the top end of the distribution.
 
 	"""
 
@@ -160,19 +231,12 @@ class Theil_T(Metric):
 
 class Theil_L(Metric):
 	"""
-	Class provides methods for calculating individual terms in the Theil L index, as well as
-	(1) a function for calculating the index itself and
-	(2) a function calculating a ratio denoting the proportion of inequality accounted for by cross-group inequality
+	Class provides methods for calculating individual terms in the Theil T index. See references for more detail.
+	The Theil index is perfectly decomposable into between- and within-group inequality. It works only using
+	positive values, which makes it unsuitable for assessing inequality across values that may take any
+	value across the entire real line (e.g. net worth, or any kind of difference).
 
-	Please note that the Theil index takes only positive values!
-
-	Reference
-	-----------
-	https://seer.cancer.gov/help/hdcalc/inference-methods/individual-level-survey-sample-1/measures-of-relative-disparity/theil-index-t
-
-	https://utip.lbj.utexas.edu/papers/utip_14.pdf
-
-	https://www.usi.edu/media/3654811/Analysis-of-Inequality.pdf
+	Relative to the Theil L, the Theil T metric is sensitive to changes at the top end of the distribution.
 
 	"""
 	def calculate(self):
@@ -271,10 +335,16 @@ class Theil_L(Metric):
 
 
 class Variance_Analysis(Metric):
-	# decomposition of income inequality by subgroups: http://www.fao.org/3/a-am342e.pdf
-	# To reduce dependencies and improve flexibility, we implement our own ANOVA
-	# should implement both bayesian (nonparametric) and frequentist (parametric) anova
-	# Please note that in this implementation, np.var calculates the POPULATION variance, not the SAMPLE variance
+	"""
+	This class offers the simplest possible decomposition of variation: it calculates population
+	variance for a provided set of values, including between- and within- decompositions and the
+	amount of variation accounted for by between-group differences.
+
+	Please note that in this implementation, np.var calculates the POPULATION variance, not the SAMPLE variance.
+	The assumption made here is that the data provided (e.g. linked education and UI records) represents the entirety
+	of the population of interest.
+
+	"""
 
 	def calculate(self):
 		self.within = self.variance_within(self.grouped_values, self.n)
@@ -285,6 +355,15 @@ class Variance_Analysis(Metric):
 
 	@staticmethod
 	def variance_within(array_of_values, n):
+		"""
+		Parameters:
+			array_of_values			:	A list of numpy arrays, each of which contains values for a particular group
+			n						:	The total number of values/observations across all groups (should be identical to np.concatenate(array_of_values).size)
+
+		Returns:
+			within_group_variance	:	A scalar identifying the total within-group variance in tha dataset
+
+		"""
 		group_variances = [np.nanvar(group) for group in array_of_values]
 		group_weights = np.asarray([len(group)/n for group in array_of_values])
 		within_group_variance = np.nansum(group_variances * group_weights)
@@ -292,19 +371,45 @@ class Variance_Analysis(Metric):
 
 	@staticmethod
 	def variance_between(array_of_values):
+		"""
+		Parameters:
+			array_of_values			:	A list of numpy arrays, each of which contains values for a particular group
+
+		Returns:
+			within_group_variance	:	A scalar identifying the total between-group variance in tha dataset
+
+		"""
 		within_group_means = [np.nanmean(group) for group in array_of_values]
 		cross_group_variance = np.nanvar(within_group_means)
 		return(cross_group_variance)
 
 	@staticmethod
 	def total_variance(observations):
+		"""
+		This is a standard variance calculation that ignores NaNs. As a sanity check, it should be identical to
+		within-group variance plus between-group variance for any dataset.
+
+		Parameters:
+			observations			:	A single numpy array
+
+		Returns:
+			total_variance			:	A scalar identifying the total variance of the provided values
+
+		"""
 		total_variance = np.nanvar(observations)
 		return(total_variance)
 
 
 class Gini(Metric):
-	# Please note as well that the Gini index is not perfectly decomposable, and contains a residual element K
-	# 
+	"""
+	The Gini index will be familiar to many users from its deployment as an income or wealth inequality metric in the settings
+	of macroeconomics, international developmnent, and comparative politics. Please note that the Gini index is NOT perfectly
+	decomposable into between- and within-group inequalities. It also contains a dimensionless residual element that is 
+	positive when individuals' relative positions to other individuals WITHIN their respective groups tend to differ from their
+	positions in the overall distribution without reference to groups.
+	
+	A .ratio attribute is also provided here, as the residual value is not meaningfully large enough to disrupt the interpretability of the ratio.
+	"""
 	def calculate(self):
 		self.within = self.gini_within(self.grouped_values)
 		self.between = self.gini_between(self.grouped_values)
@@ -314,6 +419,18 @@ class Gini(Metric):
 
 	@staticmethod
 	def gini_within(array_of_values):
+		"""
+		Available as a static method, this method allows the calculation of within-group Gini, which is an overall measure of the
+		'evenness' of a distribution of any value. Lower values denote greater equality, with 0 representing absolute equality and 1
+		representing all value accruing to a single individual.
+
+		Parameters:
+			array_of_values		:	A numpy array of numeric values
+
+		Returns:
+			G_within			:	Within-group Gini
+
+		"""
 		ungrouped_observations = np.concatenate(array_of_values).flatten()
 		n = len(ungrouped_observations)
 		group_shares = np.asarray([len(group)/n for group in array_of_values])
@@ -325,12 +442,31 @@ class Gini(Metric):
 
 	@staticmethod
 	def gini_between(array_of_values):
+		"""
+		Available as a static method, this method allows the calculation of between-group Gini, which is, generally speaking,
+		a simple measure of the dispersal of group means.
+
+		Parameters:
+			array_of_values		:	A list of numpy arrays, each containing values for a particular group
+
+		Returns:
+			G_between			:	Between-group Gini
+		"""
 		means_replaced = np.concatenate([np.repeat(np.nanmean(group), len(group)) for group in array_of_values])
 		G_between = Gini.gini(means_replaced)	
 		return(G_between)
 
 	@staticmethod
 	def gini(values):
+		"""
+		Given an array of values of any type, calculates the Gini index.
+
+		Parameters:
+			values	:	A numpy array
+
+		Returns:
+			G		:	The value of the Gini coefficient for the provided values
+		"""
 		n = len(values)
 		xbar = np.nanmean(values)
 		ad = np.array([abs(i - j) for i in values for j in values])
@@ -340,17 +476,20 @@ class Gini(Metric):
 
 class ADI(object):
 	"""
-	Methods related to the Area Deprivation Statistics
-	Data can be downloaded from: https://www.neighborhoodatlas.medicine.wisc.edu/
+	The ROI Toolkit is packaged with the Area Deprivation Statistics, which can be downloaded from: https://www.neighborhoodatlas.medicine.wisc.edu/
 
 	This object, at init, simply reads in the raw textfile of ADI indices at the block group level and forms it into quintiles.
+	It currently uses the ADI based on the 2011-2015 ACS 5-year estimates.
 
-	This currently uses the ADI based on the 2011-2015 ACS 5-year estimates.
+	The ADI data is produced at the Census block group level, but block groups containing less than 100 persons, 30 housing units, or those with >33% of pop
+	living in group quarters are suppressed with the value "PH". In this class, ADI indices are coerced to numeric, so suppressed indices are coerced to NaN.
 
-	Block groups containing less than 100 persons, 30 housing units, or >33% of pop living in group quarters are suppressed
-	with the value "PH". ADI indices are coerced to numeric, so suppressed indices are coerced to NaN.
+	At init, ADI indices are bucketed into quintiles. IMPORTANT: Lower quintiles are HIGHER-SES. Higher quintiles are more deprived.
 
-	Finally, indices are bucketed into quintiles. IMPORTANT: Lower quintiles are HIGHER-SES. Higher quintiles are more deprived.
+	Obviously, in order to use this class, you will need to have individual observations associated with Census block groups.
+	This can be accomplished with the 'Census' class in the 'external' submodule.
+
+	Instances of the ADI() class take no arguments on init - they simply read in the ADI data and prepare it for use.
 	"""
 	def __init__(self):
 		adi_location = settings.File_Locations.adi_location
